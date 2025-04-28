db81fa5ff18fb6021ddac242fce7645109cf7a03
ronb12 <ronellbradley@gmail.com>
2025-04-28 15:29:39 -0400

    Delete holiday_lawn_admin directory

--- holiday_lawn_admin/firebase-init.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID"
-};
-firebase.initializeApp(firebaseConfig);
