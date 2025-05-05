80a5f3ef54e8180e4a46c7102d0235c5274faaea
ronb12 <ronellbradley@gmail.com>
2025-05-05 07:21:02 -0400

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,14 +1,12 @@
 // firebase-init.js
-// Your Firebase Configuration and Initialization
 const firebaseConfig = {
   apiKey: "AIzaSyACm0j7I8RX4ExIQRoejfk1HZMOQRGigBw",
   authDomain: "holiday-lawn-and-garden.firebaseapp.com",
   projectId: "holiday-lawn-and-garden",
-  storageBucket: "holiday-lawn-and-garden.firebasestorage.app",
+  storageBucket: "holiday-lawn-and-garden.appspot.com", // ✅ fixed
   messagingSenderId: "135322230444",
   appId: "1:135322230444:web:1a487b25a48aae07368909",
   measurementId: "G-KD6TBWR4ZT"
 };
 
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
