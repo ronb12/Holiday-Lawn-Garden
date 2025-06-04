fd438cb4505f6646ccb2b206d60a0795d89b89fd
ronb12 <ronellbradley@gmail.com>
2025-06-04 16:30:37 -0400

    Deploy Firebase configuration for GitHub Pages - Update paths and add static config file

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+
+window.firebaseConfig = {
+  "apiKey": "AIzaSyACm0j7I8RX4ExIQRoejfk1HZMOQRGigBw",
+  "authDomain": "holiday-lawn-and-garden.firebaseapp.com",
+  "projectId": "holiday-lawn-and-garden",
+  "storageBucket": "holiday-lawn-and-garden.firebasestorage.app",
+  "messagingSenderId": "135322230444",
+  "appId": "1:135322230444:web:1a487b25a48aae07368909",
+  "measurementId": "G-KD6TBWR4ZT"
+};
+window.googleClientId = "YOUR_GOOGLE_CLIENT_ID.apps.googleusercontent.com";
